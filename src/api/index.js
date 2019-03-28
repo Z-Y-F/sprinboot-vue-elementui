d213bb5dd5959ef0f157ec7d62a0558e8eff2c1d
zhou <1220548118@qq.com>
2019-03-28 17:49:11 +0800

    SpringBoot+MongoDB+Vue+Element-ui前后端分离的前台项目

---
+++ src/api/index.js
@@ -0,0 +1,81 @@
+// 配置API接口地址
+var root = './api/v1'
+
+// 引用axios
+var axios = require('axios')
+// 自定义判断元素类型JS
+function toType (obj) {
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+}
+// 参数过滤函数
+function filterNull (o) {
+  for (var key in o) {
+    if (o[key] === null) {
+      delete o[key]
+    }
+    if (toType(o[key]) === 'string') {
+      o[key] = o[key].trim()
+    } else if (toType(o[key]) === 'object') {
+      o[key] = filterNull(o[key])
+    } else if (toType(o[key]) === 'array') {
+      o[key] = filterNull(o[key])
+    }
+  }
+  return o
+}
+/*
+  接口处理函数
+  https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
+  不同的接口的成功标识和失败提示是不一致的。
+*/
+
+function apiAxios (method, url, params, success, failure) {
+  if (params) {
+    params = filterNull(params)
+  }
+  axios({
+    method: method,
+    url: url,
+    data: method === 'POST' || method === 'PUT' ? params : params,
+    params: method === 'GET' || method === 'DELETE' ? params : params,
+    baseURL: root,
+    withCredentials: false
+  })
+
+    .then(function (result) {
+      if (result.data.success === true) {
+        if (success) {
+          success(result.data)
+        }
+      } else {
+        if (failure) {
+          failure(result.data)
+        } else {
+          window.alert('error: ' + JSON.stringify(result.data))
+        }
+      }
+    })
+
+    .catch(function (err) {
+      let res = err.response
+      if (err) {
+        window.alert('api error, HTTP CODE: ' + res.status)
+      }
+    })
+}
+
+// 返回在vue模板中的调用接口
+export default {
+  get: function (url, params, success, failure) {
+    return apiAxios('GET', url, params, success, failure)
+  },
+  post: function (url, params, success, failure) {
+    return apiAxios('POST', url, params, success, failure)
+  },
+  put: function (url, params, success, failure) {
+    return apiAxios('PUT', url, params, success, failure)
+  },
+  delete: function (url, params, success, failure) {
+    return apiAxios('DELETE', url, params, success, failure)
+  }
+}
